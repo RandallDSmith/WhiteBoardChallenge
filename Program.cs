5e9c15794cd2854a9829f78bc42948f924037295
Randall Smith <rdsmith1761@gmail.com>
2023-04-06 17:18:45 -0400

    White Board Challenge

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( FizzBuzz(15));
+            Console.WriteLine(FizzBuzz(9));
+            Console.WriteLine(FizzBuzz(5));
+            Console.WriteLine(FizzBuzz(17));
+        }
+
+        public static string FizzBuzz(int x)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (x % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (x % 5 == 0)
+            {
+                return "buzz";
+            }
+            else
+            {
+                return "Not divisible by 3 or 5";
+            }
+        }
+    }
+}
