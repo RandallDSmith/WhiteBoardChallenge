85ef633281a2ac66446594ef52590d2bccdcfe5f
Randall Smith <rdsmith1761@gmail.com>
2023-04-06 19:52:02 -0400

    WhiteBoardExercise

--- Program.cs
+++ Program.cs
@@ -15,24 +15,20 @@ namespace ArrayTest
             Console.WriteLine(FizzBuzz(17));
         }
 
-        public string FizzBuzz(int x)
+        public static string FizzBuzz(int x)
         {
-            if (x % 3 == 0 && x % 5 == 0)
+            var answer = "";
+            if (x % 3 == 0)
             {
-                return "fizzbuzz";
+                answer = "fizz";
             }
-            else if (x % 3 == 0)
+            
+            if (x % 5 == 0)
             {
-                return "fizz";
+                answer += "buzz";
             }
-            else if (x % 5 == 0)
-            {
-                return "buzz";
-            }
-            else
-            {
-                return "Not divisible by 3 or 5";
-            }
+
+            return answer;
             
         }
     }
